0ef6a2a1c8c3f0e99eacb5d1bb0cfc8416f897eb
quim96 <quim.puiggali@gmail.com>
2022-07-23 22:42:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-iv32be-setg85
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-iv32be-setg85)
---
+++ demo/users.ts
@@ -0,0 +1,19 @@
+import { colors } from '../demo-utils/colors';
+
+export const users = [
+  {
+    id: 0,
+    name: 'John smith',
+    color: colors.yellow
+  },
+  {
+    id: 1,
+    name: 'Jane Doe',
+    color: colors.blue
+  },
+  {
+    id: 2,
+    name: 'Blank user',
+    color: colors.red
+  }
+];
